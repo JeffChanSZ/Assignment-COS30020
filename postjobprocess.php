b287503e6e0c6e41d10c32400ef17f8b12ccc2b3
CHAN SIAW ZHENG <101217869@student.swin.edu.au>
2020-09-03 22:35:35 +1000

    Postjobprocess Save data to txt

--- postjobprocess.php
+++ postjobprocess.php
@@ -11,13 +11,13 @@ $errMsg = "";
 
 
 
-//Data validaton here
+//Data validaton here -- Empty Fills
   if (!empty($_POST)){
     if (isset($_POST['id']) && $_POST['id'] !="" ){
         $ID=$_POST['id'];
 
     }else {
-        echo "<p>Error: Re-enter data in <a href=\"postjobform.php\"> Post Job Form </a></p>";
+        echo "<p>Error Position ID: Empty ID Fill, Re-Enter ID in <a href=\"postjobform.php\"> Post Job Form </a></p>";
       }
     
 
@@ -25,7 +25,7 @@ $errMsg = "";
         $Title=$_POST['title'];
 
     }else {
-      echo "<p>Error: Re-enter data in <a href=\"postjobform.php\"> Post Job Form </a></p>";
+      echo "<p>Error Title: Empty Title Fill, Re-Enter Title in <a href=\"postjobform.php\"> Post Job Form </a></p>";
     }
 
     
@@ -34,7 +34,7 @@ $errMsg = "";
         $Description=$_POST['desc'];
 
     }else {
-      echo "<p>Error: Re-enter data in <a href=\"postjobform.php\"> Post Job Form </a></p>";
+      echo "<p>Error Description: Empty Description Fill, Re-Enter Description in <a href=\"postjobform.php\"> Post Job Form </a></p>";
     }
 
 
@@ -43,7 +43,7 @@ $errMsg = "";
         $Date=$_POST['date'];
 
     }else {
-      echo "<p>Error: Re-enter data in <a href=\"postjobform.php\"> Post Job Form </a></p>";
+      echo "<p>Error Date: Empty Date Fill, Re-Enter Date in <a href=\"postjobform.php\"> Post Job Form </a></p>";
     }
     
 
@@ -52,7 +52,7 @@ $errMsg = "";
         $Position=$_POST['pos'];
 
     }else {
-      echo "<p>Error: Re-enter data in <a href=\"postjobform.php\"> Post Job Form </a></p>";
+      echo "<p>Error Position: Empty Position Fill, Re-Enter Position in <a href=\"postjobform.php\"> Post Job Form </a></p>";
     }
 
         
@@ -61,38 +61,47 @@ $errMsg = "";
         $Contract=$_POST['contract'];
 
     }else {
-      echo "<p>Error: Re-enter data in <a href=\"postjobform.php\"> Post Job Form </a></p>";
+      echo "<p>Error Contract: Empty Contract Fill, Re-Enter Contract in <a href=\"postjobform.php\"> Post Job Form </a></p>";
     }
       
 
-    if (isset($_POST['app'])  && $_POST['app'] !="" ){
+    // if (isset($_POST['app'])  && $_POST['app'] !="" ){
    
-        $Application=$_POST['app'];
+    //     $Application=$_POST['app'];
 
-    }else {
-      echo "<p>Error: Re-enter data in <a href=\"postjobform.php\"> Post Job Form </a></p>";
-    }
+    // }else {
+    //   echo "<p>Error Application: Empty Application Fill, Re-Enter Application in <a href=\"postjobform.php\"> Post Job Form </a></p>";
+    // }
 
 
     if (isset($_POST['loc'])  && $_POST['loc'] !="" ){
    
         $Location=$_POST['loc'];
 
-    }else {
-        echo "<p>Error: Re-enter data in the <a href=\"postjobform.php\"> Post Job Form </a></p>";
-      }
+    }else{
+      echo "<p>Error Location: Empty Location Fill, Re-Enter Location in <a href=\"postjobform.php\"> Post Job Form </a></p>";
+    }
+
+    
+
+
+
+
+
       /**
        * IF NO Validation Error
-       */
+       **/
       saveDataToFile($ID, $Title, $Description, $Date, $Position, $Contract, $Application, $Location);
   
+  }
 
+  //Save Data to txt. files
   function saveDataToFile($ID, $Title, $Description, $Date, $Position, $Contract, $Application, $Location){
   //   echo $ID;
-  $folder = "../data/jobposts";
+  $folder = "../../data/jobposts";
   if (!is_dir($folder)) mkdir($folder, 0777, true);
 
-    $file = '../data/jobposts/jobposts.txt';
+    $file = '../../data/jobposts/jobposts.txt';
 
     $contents = $ID . $Title . $Description . $Date . $Position . $Contract . $Application . $Location ."\r\n";           
     file_put_contents($file, $contents,FILE_APPEND);     // Save our content to the file.
@@ -100,7 +109,4 @@ $errMsg = "";
   }
 
 
-
-
-  
 ?>
