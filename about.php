43f2d1bb4353c745ed316242139ab8509dc24081
CHAN SIAW ZHENG <101217869@student.swin.edu.au>
2020-09-11 14:25:14 +1000

    about css

--- about.php
+++ about.php
@@ -12,7 +12,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-		<link rel="stylesheet" href="styles/style.css" />
+        <link rel="stylesheet" href="styles/style.css" />
 	</head>
 	
 <body>
@@ -32,22 +32,36 @@
 	</div>
 
 <!--About-->
-	<section class="profiles">
-		<div class="container">
-        <div class="procol-1">
+	<section id="profiles">
+		<div id="container">
             <h1>About</h1>
 			<br>
-	
-            <?php
-                echo 'PHP version: ' . phpversion();
-            ?>
-				<p>Most of the task have been completed except for sorting of the searching list. </p>
-				<p>This is the discussion about the General Question that I've participated in this assignment 1.</p>
-
-    			<p><img class="" src="images/discussion1.png" alt="Discussion Points" width="500" height="600"></p>
-
-                <p><a href="index.php">Home</a></p>
-		</div>
+                <ul>
+                    <li><?php
+                        echo 'PHP version: ' . phpversion();
+                    ?></li>
+				    <li>Most of the tasks are completed except for Task 8 a. Properly sort the result </li>
+				    <li>Special Features ---
+                        <ol>
+                              <li>CSS: Bootstrap -- CSS Library</li>
+                              <li>PHP: file_get_contents -- Reads entire file into a string</li>
+                              <li>PHP: file_put_contents -- Write data to a file</li>
+                              <li>PHP: array_pop -- Pop the element off the end of array</li>
+                        </ol></li>
+                </ul>
+                
+                <p>
+                    <div id="about">
+                        <img id="discussion" src="images/discussion1.png" alt="Discussion Point1"></br>
+                        <p>General Question about Task 7 Advanced Search</p>
+                        <img id="discussion" src="images/discussion2.png" alt="Discussion Point2"></br>
+                        <p>General Question about Task 7 Advanced Search</p>
+                        <img id="discussion" src="images/discussion3.png" alt="Discussion Point3"></br>
+                        <p>CSS Question about self opinion and suggestion</p>
+                    </div>
+                </p>
+    			    
+                <p><a class="returnIndex" href="index.php">Home</a></p>
 		</div>
 	</section>
 
